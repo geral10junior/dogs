e6df85b0dde75f70d344545c5e9f81a116a05fb1
Geraldez Ribeiro <geraldezjunior09@gmail.com>
2025-10-04 11:55:22 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Api from "./api/Api";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
