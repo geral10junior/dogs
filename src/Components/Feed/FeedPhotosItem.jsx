55398ec2cbafff3543cc04f6e298fb96137a2e23
Geraldez Ribeiro <geraldezjunior09@gmail.com>
2025-10-11 23:34:23 -0300

    Feed modal adicionado, além da possibilidade de navegar para a aba de perfil do autor ou da publicação em si

--- src/Components/Feed/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./FeedPhotosItem.module.css";
-const FeedPhotosItem = ({ photo }) => {
+import { func } from "prop-types";
+const FeedPhotosItem = ({ photo, setModalPhoto }) => {
+  function handleClick() {
+    setModalPhoto(photo);
+  }
+
   return (
-    <li className={styles.photo}>
+    <li className={styles.photo} onClick={handleClick}>
       <img src={photo.src} alt={photo.title} />
       <span className={styles.views}>{photo.acessos}</span>
     </li>
