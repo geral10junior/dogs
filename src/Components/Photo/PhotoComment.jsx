4e0736a86d69d2a38564b6cf2c92510ae9ff8023
Geraldez Ribeiro <geraldezjunior09@gmail.com>
2025-10-13 18:56:20 -0300

    Lógica dos comentários dos posts concluído

--- src/Components/Photo/PhotoComment.jsx
+++ src/Components/Photo/PhotoComment.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import PhotoCommentsForm from "./PhotoCommentsForm";
+import { UserContext } from "../../UserContext";
+import styles from "./PhotoComments.module.css";
 
-const PhotoComment = () => {
-  return <div></div>;
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const { login } = React.useContext(UserContext);
+
+  return (
+    <>
+      <ul className={styles.comment}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm setComments={setComments} id={props.id} />}
+    </>
+  );
 };
 
-export default PhotoComment;
+export default PhotoComments;
