7f0fd9fed6d146cc3be589a290d7ed47f87e6eef
Geraldez Ribeiro <geraldezjunior09@gmail.com>
2025-10-14 18:39:31 -0300

    Animação pré imagem ser carregada

--- src/Components/Feed/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./FeedPhotosItem.module.css";
-import { func } from "prop-types";
+import Image from "../Helper/Image";
 const FeedPhotosItem = ({ photo, setModalPhoto }) => {
   function handleClick() {
     setModalPhoto(photo);
@@ -8,7 +8,8 @@ const FeedPhotosItem = ({ photo, setModalPhoto }) => {
 
   return (
     <li className={styles.photo} onClick={handleClick}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
+
       <span className={styles.views}>{photo.acessos}</span>
     </li>
   );
